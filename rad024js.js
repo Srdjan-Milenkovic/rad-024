705633732cac585d5d1e5598a48ced916634f960
Srdjan-Milenkovic <srdjan.milenkovic@politehnickasu.edu.rs>
2024-12-13 10:19:55 +0100

    Add files via upload

--- rad024js.js
+++ rad024js.js
@@ -3,16 +3,67 @@ var secondary = "#4CC842"
 var accent1 = "#CBF0FF"
 var accent2 = "#1C5719"
 
+
+
 function productmenu(){
-$(".productmenu").css({"left":"0","transition":"300ms","margin-right":"1px"})
+$(".productmenu").css({"left":"0px","transition":"300ms"})
 $("#usluge").css("color",secondary)
 $("#usluge svg").css("transform","rotate(-180deg)")
 $("#usluge svg path").css("fill",secondary)
-if($(".productmenu").css("margin-right") == "1px"){
-    $(".productmenu").css({"left":"-250px","transition":"300ms","margin-right":"0px"})
+if($(".productmenu").css("left") == "0px"){
+    $(".productmenu").css({"left":"-110vw","transition":"900ms","margin-right":"0px"})
     $("#usluge").css("color",accent1)
     $("#usluge svg").css("transform","rotate(-90deg)")
     $("#usluge svg path").css("fill", accent1)
+}
+if($(".mobilemenu").css("right") == "0px"){
+$(".mobilemenu").css({"right":"-350px","transition":"300ms"})
+$(".productmenu").css({"left":"0px","transition":"300ms"})
 
 }
 }
+
+function openmenu(){
+$(".mobilemenu").css({"right":"0px","transition":"300ms"})
+if($(".mobilemenu").css("right") == "0px"){
+    $(".mobilemenu").css({"right":"-350px","transition":"300ms"})
+}
+
+}
+
+$(window).resize(function(){
+    if($("body").width() > 1060){
+        $(".mobilemenu").css({"right":"-350px","transition":"300ms"})    
+    }
+}
+)
+
+$(document).ready(function(){
+$(".mobileitem").click(function(){
+$(".productmenu").css({"left":"-110vw","transition":"300ms"})
+$("#usluge").css("color",accent1)
+$("#usluge svg").css("transform","rotate(-90deg)")
+$("#usluge svg path").css("fill", accent1)
+
+})
+
+/*$("#usluge").hover(function(){
+$("#usluge").css({"color":secondary,"transition":"300ms"})
+$("#usluge svg path").css("fill",secondary)
+$("#usluge svg").css("transform","rotate(-180deg)")
+}
+, function(){
+   if($(".productmenu").css("left") =="0px"){
+    $("#usluge").css({"color":secondary,"transition":"300ms"})
+    $("#usluge svg path").css("fill",secondary)
+    $("#usluge svg").css("transform","rotate(-180deg)")
+   }
+   
+else{
+    $("#usluge").css({"color":accent1,"transition":"300ms"})}
+    $("#usluge svg path").css("fill",accent1)
+    $("#usluge svg").css("transform","rotate(-90deg)")
+}
+)
+*/
+})
