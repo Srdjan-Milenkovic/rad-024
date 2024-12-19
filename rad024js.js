c07384e5ea7755fc31c49b94f2ed75b7ec8edd2a
Srdjan-Milenkovic <srdjan.milenkovic@politehnickasu.edu.rs>
2024-12-19 10:12:55 +0100

    Add files via upload

--- rad024js.js
+++ rad024js.js
@@ -7,14 +7,10 @@ var accent2 = "#1C5719"
 
 function productmenu(){
 $(".productmenu").css({"left":"0px","transition":"300ms"})
-$("#usluge").css("color",secondary)
-$("#usluge svg").css("transform","rotate(-180deg)")
-$("#usluge svg path").css("fill",secondary)
+$(".usluge").toggleClass("uslugeafter")
 if($(".productmenu").css("left") == "0px"){
     $(".productmenu").css({"left":"-210vw","transition":"900ms"})
-    $("#usluge").css("color",accent1)
-    $("#usluge svg").css("transform","rotate(-90deg)")
-    $("#usluge svg path").css("fill", accent1)
+
 }
 if($(".mobilemenu").css("right") == "0px"){
 $(".mobilemenu").css({"right":"-350px","transition":"300ms"})
@@ -40,29 +36,7 @@ $(window).resize(function(){
 $(document).ready(function(){
 $(".mobileitem").click(function(){
 $(".productmenu").css({"left":"-210vw","transition":"900ms"})
-$("#usluge").css("color",accent1)
-$("#usluge svg").css("transform","rotate(-90deg)")
-$("#usluge svg path").css("fill", accent1)
-
+$(".usluge").toggleClass("uslugeafter")
 })
 
-/*$("#usluge").hover(function(){
-$("#usluge").css({"color":secondary,"transition":"300ms"})
-$("#usluge svg path").css("fill",secondary)
-$("#usluge svg").css("transform","rotate(-180deg)")
-}
-, function(){
-   if($(".productmenu").css("left") =="0px"){
-    $("#usluge").css({"color":secondary,"transition":"300ms"})
-    $("#usluge svg path").css("fill",secondary)
-    $("#usluge svg").css("transform","rotate(-180deg)")
-   }
-   
-else{
-    $("#usluge").css({"color":accent1,"transition":"300ms"})}
-    $("#usluge svg path").css("fill",accent1)
-    $("#usluge svg").css("transform","rotate(-90deg)")
-}
-)
-*/
 })
